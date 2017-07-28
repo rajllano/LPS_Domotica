bc99c610458f1860caf64137102355884fa78859
Max <Max@DESKTOP-3NTQF19>
2017-07-27 22:02:47 -0500

    Primera version

---
+++ lps_domotica_modelo/Administrativo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lps_domotica_modelo
+{
+    public class Administrativo
+    {
+    }
+}
---
+++ lps_domotica_modelo/Domotica.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lps_domotica_modelo
+{
+    public class Domotica
+    {
+        private static Domotica Instancia;
+
+        public static Domotica getInstancia()
+        {
+            if (Instancia == null)
+                Instancia = new Domotica();
+
+            return Instancia;
+        }
+
+        public Domotica()
+        {
+            Administrativo = new Administrativo();
+            Seguridad = new Seguridad();
+        }
+
+        public Seguridad Seguridad
+        {
+            get; set;
+        }
+
+        public Administrativo Administrativo
+        {
+            get; set;
+        }
+    }
+}
